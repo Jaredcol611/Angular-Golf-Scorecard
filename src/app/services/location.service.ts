0e967ad02b28a038b10c9d5e10d20e8c82944729
Jared <jaredcol13@gmail.com>
2018-01-30 23:50:28 -0700

    Missing files commited

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,24 @@
+// import { Injectable } from '@angular/core';
+// import { LocationCoordinates} from '../LocationCoordinates';
+// import {Observable} from 'rxjs/Observable';
+// import {HttpClient} from '@angular/common/http';
+//
+//
+// @Injectable()
+// export class LocationService {
+//
+//   key = 'AIzaSyCDt-DBTk2MCvsZZI_9pB7IInyxu3pJt0Y';
+//
+//   constructor(private httpClient: HttpClient) { }
+//
+//   getLocation(zipCode): Observable<LocationCoordinates[]> {
+//     return this.httpClient.get('https://maps.googleapis.com/maps/api/geocode/json?address='+zipCode+'&key='+key+'', function(data){
+//       let zipSearch = data;
+//       console.log(data);
+//     })
+//
+// }
+
+
+
+
